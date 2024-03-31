9d4c7d27ee257c5af6bf9f7475a9ae12f2f02bf4
hozgurdogan <hasanmehmet84@gmail.com>
2024-03-31 14:18:43 +0300

    ilk Commit

---
+++ data structures/data_structers.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Data Structers Quick Sum
+
+#int
+x=12
+type(x)
+
+#float
+x=12.2
+type(x)
+
+#String
+x="Sting Data"
+type (x)
+
+#Bollean
+x=True
+type(x)
+3==2
+
+#List
+
+x=["btc","eth","xrp"]
+type(x)
+
+#dictionary
+#Key,value
+x={"Name":"Ozgur", "Age":22}
+type(x)
+
+
+#Tuple
+x=("ptyhon","ml","ds")
+type(x)
+
+
+#Set
+x={"ptyhon","ml","ds"}
+type(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
