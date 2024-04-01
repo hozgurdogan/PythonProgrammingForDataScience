9ed3e347127314036b47a89bf9034a6e836a0d90
hozgurdogan <hasanmehmet84@gmail.com>
2024-04-01 21:55:31 +0300

    About Strings

---
+++ data structures/strings.py
@@ -0,0 +1,39 @@
+# Karakter Dizileri (Strings)
+
+#####################################
+
+print("Jhon")
+
+print('jhon')
+
+"Jhon"
+
+name= "Jhon"
+print(name)
+
+
+###################################
+# Multi Rows Strings Arrays
+###################################
+
+
+long_Str=""" Veri Yapıları : Hızlı Özet,
+Sayılar (Numbers) : int ,float, complex,
+Karakter Dizileri (Strings) : str,
+List , Disctionary , Tuple ,Set,
+Boolean (TRUE-FALSE)": bool"""
+
+#Karater dizileri elemalarına ulaşmak 
+
+name="Name"
+
+name[0]
+name[1]
+name[2]
+name[3]
+
+# Karatker dizilerinde Slice işlemi
+name="John"
+name[0:2]
+
+long_Str[0:10]
